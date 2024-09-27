bafac74a8a68aeb41f6143280fe8a22abf7faedf
lumen <lumen2004.scholar@gmail.com>
2024-09-27 10:09:03 +0800

    perf: deprecate sequential training.

--- scripts/long_ttt/train.py
+++ scripts/long_ttt/train.py
@@ -10,7 +10,7 @@ from typing import Optional
 from copy import deepcopy
 
 
-def train(dataset: Dataset, tokenizer: PreTrainedTokenizer, training_args: TrainingArguments, enable_sequential_training: bool=False, sequential_training_epochs: Optional[int]=None, **kwargs):
+def train(dataset: Dataset, tokenizer: PreTrainedTokenizer, training_args: TrainingArguments, **kwargs):
     """Fine-tune the model and the corresponding tokenizer.
     Args:
         dataset (Dataset): the dataset to train on.
@@ -33,16 +33,6 @@ def train(dataset: Dataset, tokenizer: PreTrainedTokenizer, training_args: Train
     # Load and finetune the model
     trainer, model = load_trainer(dataset, tokenizer, training_args, **kwargs)
     trainer.train()
-    # Post-sequential training
-    if enable_sequential_training:
-        seq_training_args = deepcopy(training_args)
-        seq_kwargs = deepcopy(kwargs)
-        seq_training_args.num_train_epochs = sequential_training_epochs
-        seq_training_args.per_device_train_batch_size = 1
-        seq_training_args.gradient_accumulation_steps = 1
-        seq_kwargs['gather_batches'] = False
-        seq_trainer, model = load_trainer(dataset, tokenizer, seq_training_args, model=trainer.model, optimizer=trainer.optimizer, **seq_kwargs)  # Load the previous model and optimizer
-        seq_trainer.train()
     for param in model.parameters():
         if param.requires_grad:
             param.grad = None
