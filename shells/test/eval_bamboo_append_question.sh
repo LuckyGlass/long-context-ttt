77b2f06eb18f73b2bc4a3c09bad8f1bc7e078fad
lumen <lumen2004.scholar@gmail.com>
2024-10-23 21:33:07 +0800

    test: test TTT on Bamboo under append-question setting.

---
+++ shells/test/eval_bamboo_append_question.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH -J bamboo
+#SBATCH -N 1
+#SBATCH -p IAI_SLURM_HGX
+#SBATCH --gres=gpu:4
+#SBATCH --qos=16gpu-hgx
+#SBATCH --time=72:00:00
+#SBATCH -o logs/%j-bamboo-append-question-out.log
+#SBATCH -e logs/%j-bamboo-append-question-err.log
+#SBATCH -c 1
+
+wandb disabled
+python scripts/prediction_bamboo.py \
+    --remove_unused_columns False \
+    --model_name_or_path models/Meta-Llama-3-8B-Instruct \
+    --output_dir models/temp \
+    --overwrite_output_dir True \
+    --per_device_train_batch_size 1 \
+    --learning_rate 1e-6 \
+    --weight_decay 1e-4 \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --num_train_epochs 0 \
+    --log_level info \
+    --logging_strategy steps \
+    --logging_steps 1 \
+    --save_strategy no \
+    --bf16 True \
+    --tf32 False \
+    --gradient_checkpointing True \
+    --full_ft True \
+    --gather_batches True \
+    --block_size 256 \
+    --len_segment 8 \
+    --len_offset 3 \
+    --model_max_length 7900 \
+    --pad_to_max_length False \
+    --enable_ICL True \
+    --recite_first False \
+    --output_file outputs/bamboo-ttt-append-question.jsonl \
+    --input_file datasets/bamboo/reportsumsort_16k.jsonl \
+    --append_question True
