66c94dc77150c863727a2701833a2407f1adb6e1
lumen <lumen2004.scholar@gmail.com>
2024-11-13 20:00:47 +0800

    test: ICL+SFT+TTT, w/o AT.

---
+++ shells/test/Bamboo-SFTSent-FullTTT.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH -J Bamboo
+#SBATCH -N 1
+#SBATCH -p IAI_SLURM_HGX
+#SBATCH --gres=gpu:4
+#SBATCH --qos=16gpu-hgx
+#SBATCH --time=72:00:00
+#SBATCH -o logs/%j-Bamboo-SFTSent-FullTTT.out.log
+#SBATCH -e logs/%j-Bamboo-SFTSent-FullTTT.err.log
+#SBATCH -c 1
+
+wandb disabled
+python scripts/prediction_bamboo.py \
+    --tokenizer_name_or_path models/Meta-Llama-3-8B-Instruct \
+    --model_name_or_path models/sft-sent-fixed-merge \
+    --model_max_length 7900 \
+    --output_file outputs/Bamboo-SFTSent-FullTTT.jsonl \
+    --overwrite True \
+    --input_file datasets/bamboo/reportsumsort_16k.jsonl \
+    --block_size 256 \
+    --len_segment 8 \
+    --len_offset 3 \
+    --pad_to_max_length False \
+    --full_ft True \
+    --gather_batches True \
+    --num_train_epochs 5 \
+    --remove_unused_columns True \
+    --report_to none \
+    --output_dir models/temp \
+    --overwrite_output_dir True \
+    --per_device_train_batch_size 1 \
+    --learning_rate 1e-6 \
+    --weight_decay 1e-4 \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --log_level info \
+    --logging_strategy steps \
+    --logging_steps 1 \
+    --save_strategy no \
+    --bf16 True \
+    --tf32 False \
+    --gradient_checkpointing True \
---
+++ shells/test/LooGLET-SFTSent-FullTTT.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -J LooGLET
+#SBATCH -N 1
+#SBATCH -p IAI_SLURM_HGX
+#SBATCH --gres=gpu:4
+#SBATCH --qos=16gpu-hgx
+#SBATCH --time=72:00:00
+#SBATCH -o logs/%j-LooGLET-SFTSent-FullTTT.out.log
+#SBATCH -e logs/%j-LooGLET-SFTSent-FullTTT.err.log
+#SBATCH -c 1
+
+python scripts/prediction_loogle.py \
+    --tokenizer_name_or_path models/Meta-Llama-3-8B-Instruct \
+    --model_name_or_path models/sft-sent-fixed-merge \
+    --model_max_length 7900 \
+    --output_file outputs/LooGLET-SFTSent-FullTTT.json \
+    --input_file datasets/loogle/loogle-timeline-reorder.json \
+    --block_size 256 \
+    --len_segment 8 \
+    --len_offset 3 \
+    --pad_to_max_length False \
+    --full_ft True \
+    --gather_batches True \
+    --num_train_epochs 5 \
+    --remove_unused_columns True \
+    --report_to none \
+    --output_dir models/temp \
+    --overwrite_output_dir True \
+    --per_device_train_batch_size 1 \
+    --learning_rate 1e-6 \
+    --weight_decay 1e-4 \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --log_level info \
+    --logging_strategy steps \
+    --logging_steps 1 \
+    --save_strategy no \
+    --bf16 True \
+    --tf32 False \
+    --gradient_checkpointing True \
